fb1a1e43950b02cd80dbc1385e624ee96ed6257f
Agrim Asthana <dev@agrimasthana.com>
2017-07-31 21:07:22 -0500

    first draft parallel execution

--- src/main/scala/com/a10a/spark/brewery/SparkRDD.scala
+++ src/main/scala/com/a10a/spark/brewery/SparkRDD.scala
@@ -1,14 +1,13 @@
-//import com.mongodb.spark.MongoSpark
-import com.mongodb.spark.MongoSpark
+package com.a10a.spark.brewery
+
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SparkSession
+
 
 object SparkRDD {
   def main(args: Array[String]) = {
-      val conf = new SparkConf().setAppName("Brewery2").setMaster("local[4]")
+      val conf = new SparkConf().setAppName("Brewery").setMaster("spark://192.168.0.200:7077")
       val sc = new SparkContext(conf)
       val numbers = 1 to 10
-      println(sc.getConf)
       val rdd = sc.parallelize(numbers)
       rdd.foreach(println)
   }
