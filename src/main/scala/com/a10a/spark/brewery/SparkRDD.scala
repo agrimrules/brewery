7c5a6fdeb77039bd75a895a11c155b84e714d265
Agrim Asthana <dev@agrimasthana.com>
2017-08-09 21:05:36 -0500

    added save to mongo hook

--- src/main/scala/com/a10a/spark/brewery/SparkRDD.scala
+++ src/main/scala/com/a10a/spark/brewery/SparkRDD.scala
@@ -5,14 +5,14 @@ import com.mongodb.spark._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.concat
 import org.apache.spark.sql.functions.lit
-import com.mongodb.spark.config.WriteConfig
-
+import com.mongodb.spark.config._
+import org.bson.Document
 
 object SparkRDD {
   def main(args: Array[String]) = {
       val (inmongo, outmongo) = utils.getMongoConf()
       val sc = SparkSession.builder()
-                  .master("local[*]")
+                  .master("local[8]")
                   .appName("Brewery")
                   .config("spark.mongodb.input.uri", inmongo)
                   .config("spark.mongodb.output.uri", outmongo)
@@ -28,6 +28,8 @@ object SparkRDD {
         .drop("brand").drop("name")
       val formatted: DataFrame = trimmed.select("ts","Beer Name", "percentage").distinct().sort("ts")
       formatted.show()
+      val writeConfig = WriteConfig(Map("collection" -> formatted.first().getAs("Beer Name").toString, "writeConcern.w" -> "majority"), Some(WriteConfig(sc)))
+      MongoSpark.save(formatted, writeConfig)
       // Do machine learning on formatted data
     })
 
